cfc9d8cd7aee9fe6f57588b54da64d1f9cbb1f94
ndorrh <ndorrh@gmail.com>
2023-03-01 19:15:43 +0100

    Add greeting component

---
+++ src/components/greeting.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Greeting = ({prop}) => {
+
+  console.log(prop)
+  if (prop != undefined && prop != null) {
+    return (
+      <div className="greeting">
+        <h2>Message</h2>
+        <p><span>Text:</span> {prop.content}</p>
+        <p><span>Created at:</span> {prop.created_at}</p>
+      </div>
+    );
+  } else {
+    return (
+      <div className="greeting">
+        <h2>Message</h2>
+        <p>No message to display</p>
+      </div>
+    );
+  }
+}
+
+export default Greeting
