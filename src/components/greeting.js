620a4061f088221c1080e2756c9a31b83b2cd5ed
ndorrh <ndorrh@gmail.com>
2023-03-01 19:56:23 +0100

    Fix linter errors

--- src/components/greeting.js
+++ src/components/greeting.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Greeting = ({prop}) => {
+const Greeting = ({ prop }) => {
 
   console.log(prop)
   if (prop != undefined && prop != null) {
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
+import reportWebVitals from './reportWebVitals';
+import App from './App';
 import store from './redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
